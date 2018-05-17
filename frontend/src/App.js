ff7717f83ecf3fdb58f92651e37934e399cbe379
Henrique Varella Ehrenfried <h.v.ehrenfried@gmail.com>
2018-05-17 21:18:31 +0000

    Build the skeleton of the site
    Signed-off-by: Henrique Varella Ehrenfried <h.v.ehrenfried@gmail.com>

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,46 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import {Col, Row, Button, Navbar, Card, Input, Icon} from 'react-materialize'
+
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+        <div className='git_fetcher'>
+          <Navbar brand='GIT FETCHER' fixed={true} className='navbar-header'/>
+          <Row className='item_card '>
+            <Col m={12} s={12}>
+                <Card className='white' textClassName='black-text' title='GitHub repositories languages'>
+                  <p>
+                    Chose your languages
+                  </p>
+                  <Row className='insed_item_card'>
+                    <Col offset={'m1'} s={12} m={2}>
+                      <Input s={12} label="First Language" />
+                    </Col>
+                    <Col s={12} m={2}>
+                      <Input s={12} label="Second Language" />
+                    </Col>
+                    <Col s={12} m={2}>
+                      <Input s={12} label="Third Language" />
+                    </Col>
+                    <Col s={12} m={2}>
+                      <Input s={12} label="Fourth Language" />
+                    </Col>
+                    <Col s={12} m={2}>
+                      <Input s={12} label="Fifth Language" />
+                    </Col>
+                  </Row>
+                  <Row>
+                    <Button className='right primaryBackgroundColor search-btn'>
+                      <Icon left>search</Icon>
+                      Search for repositories
+                    </Button>
+                  </Row>
+                </Card>
+            </Col>
+          </Row>
+        </div>
     );
   }
 }
---
+++ frontend/src/enviroment.js
@@ -0,0 +1,3 @@
+// Example: api.github.com/search/repositories?q=language:Python,+language:Ruby&s=star
+
+export const GITHUB_API = 'api.github.com';
